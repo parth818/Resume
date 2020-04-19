9fb9a85343b6f8e1a6950b7f1291e0a66eda2c31
parth818 <vermaparth818@gmail.com>
2020-04-20 00:53:59 +0530

    updated 20-04-2020

---
+++ js/auto_scroll.js
@@ -0,0 +1,33 @@
+var currentpos=0,alt=1,curpos1=0,curpos2=-1
+function initialize(){
+startit()
+}
+function scrollwindow(){
+if (document.all)
+temp=document.body.scrollTop
+else
+temp=window.pageYOffset
+if (alt==0)
+alt=1
+else
+alt=0
+if (alt==0)
+curpos1=temp
+else
+curpos2=temp
+if (curpos1!=curpos2){
+if (document.all)
+currentpos=document.body.scrollTop+1
+else
+currentpos=window.pageYOffset+1
+window.scroll(0,currentpos)
+}
+else{
+currentpos=0
+window.scroll(0,currentpos)
+}
+}
+function startit(){
+setInterval("scrollwindow()",40)
+}
+window.onload=initialize
