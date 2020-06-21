c8df8054cb1a035ee5da67f7574fccdee5abe07d
parth818 <vermaparth818@gmail.com>
2020-06-21 14:40:20 +0530

    updated_21_06_2020

--- js/loader.js
+++ js/loader.js
@@ -1,4 +1,6 @@
 window.addEventListener("load", function(){
    const loader_element = document.querySelector(".preloader");
    loader_element.className += " hidden";
+   body_element = document.querySelector(".bg");
+   body_element.className += " scroll_y";
 })
